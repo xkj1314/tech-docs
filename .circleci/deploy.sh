e52ee91cfa82f10eb7fab6864fc1d60347195967
Yuya Ushijima <yuya.ushijima@g.softbank.co.jp>
2019-06-03 18:17:36 +0900

    shuusei

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,20 +6,18 @@ set -e
 DEPLOY_DIR=deploy
 
 # gitの諸々の設定
-#git config --global push.default simple
-#git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
-#git config --global user.name $CIRCLE_USERNAME
+git config --global push.default simple
+git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
+git config --global user.name $CIRCLE_USERNAME
 
 # gh-pagesブランチをdeployディレクトリにクローン
 git clone -q --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
 
 # rsyncでhugoで生成したHTMLをコピー
 cd $DEPLOY_DIR
-# rsync -arv --delete ../public/* .
-pwd
+rm -rf ./*
 rsync -arv --delete ../docs/* .
 ls -tlar
-pwd
 
 git add -f .
 git commit -m "Deploy #$CIRCLE_BUILD_NUM from CircleCI [ci skip]" || true
